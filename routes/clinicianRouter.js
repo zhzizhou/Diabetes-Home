0bf42e00fa782657f2b5c57438f187663c967b85
Brian Zhang <zhzizhou@gmail.com>
2022-04-20 13:06:24 +1000

    updated clinician controller

--- routes/clinicianRouter.js
+++ routes/clinicianRouter.js
@@ -7,6 +7,7 @@ clinicianRouter.get('/home', clinicianController.getHome)
 
 //display clinician's profile page
 clinicianRouter.get('/profile', clinicianController.getProfile)
+    //clinicianRouter.get('/:clinician_id', clinicianController.getProfile)
 
 //display clinician's edit profile page
 clinicianRouter.get('/edit', clinicianController.getEditPage)
