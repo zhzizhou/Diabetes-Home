9f3e4e6567068a2811b560f84e0a37db26b7c9b1
YuHinLi <yuhinl@student.unimelb.edu.au>
2022-04-23 14:35:28 +1000

    update patient enter hbs

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const HealthRecord = require('../models/healthRecord')
-const LogItem = require('../models/logItem')
+const Patient = require('../models/patient')
+const moment = require('moment')
 const expressValidator = require('express-validator')
 
 const getHome = async(req, res) => {
@@ -21,19 +22,46 @@ const getLogHistory = async(req, res) => {
 const getLogPage = async(req, res) => {
     console.log('GET Patient LogPage')
 
+    var pID = "625e1e3d67c164c3d21e5bce"
+    var logName
+    var when = moment(new Date()).format('D/M/YY H:mm:ss')
+
+    console.log(req.params.id)
+
+    if (req.params.id != '') {
+        switch (req.params.id) {
+            case '1':
+                logName = "Weight"
+                break
+            case '2':
+                logName = "Insulin Doses"
+                break
+            case '3':
+                logName = "Exercise"
+                break
+            case '4':
+                logName = "Blood Glucose Leve"
+                break
+        }
+    }
+
+
     try {
-        const logItem = await LogItem.findById(
-            // req.params.logItemId
-            // "625e240b01e5ce1b9ef808e9"
+        const patient = await Patient.findById(
+            pID
         ).lean()
 
-        if (!logItem) {
+        if (!patient) {
             return res.sendStatus(404)
         }
 
-        //found clinician
-        return res.render('patient-enter-ts', {
-            thisLogItem: logItem
+
+
+        //found patient
+        return res.render('patient-enter-hs', {
+            thisPatient: patient,
+            title: logName,
+            time: when
         })
 
     } catch (err) {
