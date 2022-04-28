f83321878fd3951ad106660c58003ebe77d2fed3
MarcoLIU27 <lhy1421381989@gmail.com>
2022-04-28 19:31:59 +1000

    update enter-hs

--- public/js/pop-up-window.js
+++ public/js/pop-up-window.js
@@ -1,9 +1,30 @@
+
+
 function showModel() {
     var model = document.getElementById("successful-update-data");
     model.style.display = "block";
+    //var body = document.getElementById("ts-body");
+    //body.style.overflow = "hidden";
+    stopBodyScroll(1);
 }
 
 function closeModel() {
     var model = document.getElementById("successful-update-data");
     model.style.display = "none";
+}
+
+function stopBodyScroll(isFixed) {
+    let bodyEl = document.body;
+    let top = 0;
+    
+    if (isFixed) {
+        top = window.scrollY;
+        bodyEl.style.position = 'fixed';
+        bodyEl.style.top = -top + 'px';
+    }
+    else {
+        bodyEl.style.position = '';
+        bodyEl.style.top = '';
+        window.scrollTo(0, top);
+    }
 }
