2e3222843f54966cbf4dd6df1ed30e7d33ce226d
YuHinLi <yuhinl@student.unimelb.edu.au>
2022-05-06 14:00:36 +1000

    update method for note in clinician patient page

--- controllers/clinicianController.js
+++ controllers/clinicianController.js
@@ -525,6 +525,9 @@ const getPatientDetail = async(req, res) => {
 
     var cId = '625e240b01e5ce1b9ef808e9'
     try {
+        const clinician = await Clinician.findById(
+            cId
+        ).lean()
         const patient = await Patient.findById(req.params.id).lean()
         patient.age = utility.getAge(patient.dateOfBirth)
             //search all health record group by date in descending order
@@ -583,7 +586,8 @@ const getPatientDetail = async(req, res) => {
         //     notes[j].when = moment(notes[j].when).format('D/M/YY H:mm:ss')
         // }
 
-        const notes = await clinicianNote.findOne({
+
+        const notes = await clinicianNote.find({
             patientId: req.params.id,
             clinicianId: cId
         }).lean()
