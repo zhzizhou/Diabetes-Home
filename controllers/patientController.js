797f82eca6defbff405561a621e1e3a3c9f26a1d
YuHinLi <yuhinl@student.unimelb.edu.au>
2022-05-20 18:25:19 +1000

    Revert "dark mode broken"
    This reverts commit 685094ab54c3527c4d91c576deed0e60b4055861.

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -571,16 +571,12 @@ const getSettings = async(req, res) => {
         if (!patient) {
             return res.sendStatus(404)
         }
-        
-        var light
         var colorlayout
         if (patient.darkMode == false) {
             //light colorscheme
             colorlayout = 'patient-changepassword'
-            light = true 
         } else {
             colorlayout = 'DARK-patient-changepassword'
-            light = false
         }
 
         //found patient
@@ -588,8 +584,7 @@ const getSettings = async(req, res) => {
             layout: colorlayout,
             thisTitle: "Settings",
             thisPatient: patient,
-            icon: "bloodtype",
-            light: light 
+            icon: "bloodtype"
         })
 
     } catch (err) {
